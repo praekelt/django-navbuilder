4de50d5f88a95f20903bf083b61f5bd7ae249437
qoda <jpbydendyk@gmail.com>
2016-08-31 13:49:10 +0200

    Added fields to admin forms

--- navbuilder/forms.py
+++ navbuilder/forms.py
@@ -7,8 +7,10 @@ from navbuilder.models import Menu, MenuItem
 class MenuAdminForm(forms.ModelForm):
     class Meta:
         model = Menu
+        fields = ["title", "slug"]
 
 
 class MenuItemAdminForm(forms.ModelForm):
     class Meta:
         model = MenuItem
+        fields = ["title", "slug", "position", "menu", "parent", "link"]
