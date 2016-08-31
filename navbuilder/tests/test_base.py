189aaf15ef1c11b08cbc87bdc8e85d7a0e7ff07a
qoda <jpbydendyk@gmail.com>
2016-08-31 11:35:35 +0200

    Stubbed unittests

--- navbuilder/tests/test_base.py
+++ navbuilder/tests/test_base.py
@@ -1,4 +1,50 @@
-# -*- coding: utf-8 -*-
-
+from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.test.client import Client, RequestFactory
+from django.test.client import Client
+
+from navbuilder import models
+
+
+class ModelTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_link(self):
+        pass
+
+
+class AdminTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.editor = get_user_model().objects.create(
+            username="editor",
+            email="editor@test.com",
+            is_superuser=True,
+            is_staff=True
+        )
+        self.editor.set_password("password")
+        self.editor.save()
+        self.client.login(username="editor", password="password")
+
+    def test_admin(self):
+        pass
+
+    def test_admin_link(self):
+        pass
+
+    def tearDown(self):
+        self.client.logout()
+
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_detail(self):
+        pass
+
+    def test_list(self):
+        pass
+
+    def tearDown(self):
+        pass
