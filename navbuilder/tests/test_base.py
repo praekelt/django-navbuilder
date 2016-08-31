7909ebc6c6ccdc66a7f6662a07f9e14b7f631205
qoda <jpbydendyk@gmail.com>
2016-08-31 13:50:25 +0200

    Added rudimentary unittests for models

--- navbuilder/tests/test_base.py
+++ navbuilder/tests/test_base.py
@@ -7,10 +7,45 @@ from navbuilder import models
 
 class ModelTestCase(TestCase):
     def setUp(self):
-        pass
+        self.menu_data = {
+            "title": "Menu 1",
+            "slug": "menu-1"
+        }
+        self.menu = models.Menu.objects.create(**self.menu_data)
+
+        self.link_data = {
+            "title": "Menu 1",
+            "slug": "menu-1",
+            "url": "/link/1/"
+        }
+        self.link = models.LINK_MODEL.objects.create(**self.link_data)
+
+        self.menuitem_data = {
+            "title": "Menu Item 1",
+            "slug": "menu-item-1",
+            "position": 1,
+            "menu": self.menu,
+            "link": self.link
+        }
+        self.menuitem = models.MenuItem.objects.create(**self.menuitem_data)
 
     def test_link(self):
-        pass
+
+        # ensure the data was saved correctly
+        for key, value in self.link_data.items():
+            self.assertEqual(getattr(self.link, key), value)
+
+    def test_menu(self):
+
+        # ensure the data was saved correctly
+        for key, value in self.menu_data.items():
+            self.assertEqual(getattr(self.menu, key), value)
+
+    def test_menuitem(self):
+
+        # ensure the data was saved correctly
+        for key, value in self.menuitem_data.items():
+            self.assertEqual(getattr(self.menuitem, key), value)
 
 
 class AdminTestCase(TestCase):
