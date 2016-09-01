bef26b53ed0b03ae7ff237b402ad74e28a57df21
qoda <jpbydendyk@gmail.com>
2016-09-01 16:40:58 +0200

    Prepopulate the slug field from title

--- navbuilder/admin.py
+++ navbuilder/admin.py
@@ -7,12 +7,14 @@ from navbuilder.models import Menu, MenuItem
 class MenuItemInline(admin.StackedInline):
     form = MenuItemAdminForm
     model = MenuItem
+    prepopulated_fields = {"slug": ["title"]}
 
 
 class MenuAdmin(admin.ModelAdmin):
     form = MenuAdminForm
     list_display = ["title"]
     inlines = [MenuItemInline]
+    prepopulated_fields = {"slug": ["title"]}
 
 
 admin.site.register(Menu, MenuAdmin)
