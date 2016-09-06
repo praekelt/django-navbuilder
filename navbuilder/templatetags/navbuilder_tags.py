b9c9e1c3c40c05b775ddd607570739652bf05dc2
qoda <jpbydendyk@gmail.com>
2016-09-06 09:50:24 +0200

    Added navbuilder url pattern to test urls

--- navbuilder/templatetags/navbuilder_tags.py
+++ navbuilder/templatetags/navbuilder_tags.py
@@ -1,5 +1,5 @@
 from django import template
-
+from django.template.loader import render_to_string
 
 register = template.Library()
 
@@ -8,5 +8,13 @@ register = template.Library()
     "navbuilder/inclusion_tags/menu_detail.html", takes_context=True
 )
 def render_menu(context, obj):
+    context["object"] = obj
+    return context
+
+
+@register.inclusion_tag(
+    "navbuilder/inclusion_tags/menuitem_detail.html", takes_context=True
+)
+def render_menuitem(context, obj):
     context["object"] = obj
     return context
--- navbuilder/tests/urls.py
+++ navbuilder/tests/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r"^admin/", include(admin.site.urls))
+    url(r"^admin/", include(admin.site.urls)),
+    url(r"^navbuilder/", include("navbuilder.urls", namespace="navbuilder")),
 ]
