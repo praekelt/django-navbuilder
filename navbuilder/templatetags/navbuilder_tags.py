c4720ab4d538fd386be79920c3ec64e7cb64f74f
Johan Beyers <johan@juizi.com>
2016-12-14 09:37:12 +0200

    Add ability to get breadcrumbs from menus

--- navbuilder/templatetags/navbuilder_tags.py
+++ navbuilder/templatetags/navbuilder_tags.py
@@ -1,6 +1,7 @@
 from django import template
+from django.contrib.contenttypes.models import ContentType
 
-from navbuilder.models import Menu
+from navbuilder.models import Menu, MenuItem
 
 register = template.Library()
 
@@ -21,4 +22,45 @@ def render_menu(context, slug):
 )
 def render_menuitem(context, obj):
     context["object"] = obj
+    return context
+
+
+@register.inclusion_tag(
+    "navbuilder/inclusion_tags/breadcrumbs.html", takes_context=True
+)
+def navbuilder_breadcrumbs(context, slug):
+    """
+    Render the breadcrumbs, based on the current object. Prefer using the
+    structure of the menu designated by slug, but use any menu available.
+    Typical use case for this would be if the main menu has an about/terms
+    page, but it's mirrored in the footer menu in a much flatter layout. We
+    prefer the main menu structure. This also allows us to construct
+    breadcrumbs for items that don't show up in page menus at all.
+    """
+    context["navbuilder_breadcrumbs"] = []
+    if "object" not in context:
+        return context
+
+    def get_menuitems(item):
+        if item.parent:
+            struct = get_menuitems(item.parent)
+            struct.append(item)
+            return struct
+        return [item]
+
+    content_type = ContentType.objects.get_for_model(context["object"])
+    crumb_sets = []
+    for item in MenuItem.objects.filter(
+            link_content_type__pk=content_type.id,
+            link_object_id = context["object"].id):
+
+        crumb_sets.append(get_menuitems(item))
+
+    for crumb_set in crumb_sets:
+        menu = crumb_set[0].menu
+        if menu and menu.slug == slug:
+            context["navbuilder_breadcrumbs"] = crumb_set
+
+    if not context["navbuilder_breadcrumbs"]:
+        context["navbuilder_breadcrumbs"] = crumb_sets[0]
     return context
--- navbuilder/tests/test_base.py
+++ navbuilder/tests/test_base.py
@@ -37,3 +37,39 @@ def load_fixtures(kls):
     kls.sub_menuitem = models.MenuItem.objects.create(
         **kls.sub_menuitem_data
     )
+
+
+def load_crumb_fixtures(kls):
+    kls.menu_data_2 = {
+        "title": "Menu 2",
+        "slug": "menu-2"
+    }
+    kls.menu_2 = models.Menu.objects.create(**kls.menu_data_2)
+
+    kls.link_data_2 = {
+        "title": "Link 2",
+        "slug": "link-2",
+        "url": "/link/2/"
+    }
+    kls.link_2 = Link.objects.create(**kls.link_data_2)
+
+    kls.menuitem_data_2 = {
+        "title": "Menu Item 2",
+        "slug": "menu-item-2",
+        "position": 2,
+        "menu": kls.menu_2,
+        "link": kls.link_2
+    }
+    kls.menuitem_2 = models.MenuItem.objects.create(**kls.menuitem_data_2)
+
+    kls.sub_menuitem_data_2 = {
+        "title": "Sub Menu Item 2",
+        "slug": "sub-menu-item-2",
+        "position": 2,
+        "parent": kls.menuitem_2,
+        "target": "blank",
+        "link": kls.link_2
+    }
+    kls.sub_menuitem_2 = models.MenuItem.objects.create(
+        **kls.sub_menuitem_data_2
+    )
